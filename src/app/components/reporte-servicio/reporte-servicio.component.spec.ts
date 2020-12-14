1a2f6df56fbe4135d3317aca8e07d5569b200c83
Christian Quintero <chriswbe1993@gmail.com>
2020-12-14 15:47:43 -0500

    Actualización de angular

--- src/app/components/reporte-servicio/reporte-servicio.component.spec.ts
+++ src/app/components/reporte-servicio/reporte-servicio.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ReporteServicioComponent } from './reporte-servicio.component';
 
@@ -6,7 +6,7 @@ describe('ReporteServicioComponent', () => {
   let component: ReporteServicioComponent;
   let fixture: ComponentFixture<ReporteServicioComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReporteServicioComponent ]
     })
