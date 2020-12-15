e60b2645f0dd524b77a9f573998134fd292e1f67
Christian Quintero <chriswbe1993@gmail.com>
2020-12-15 00:04:10 -0500

    Se agregan datos de prueba para visualización

--- src/app/components/horas-trabajo/horas-trabajo.component.ts
+++ src/app/components/horas-trabajo/horas-trabajo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IServicio } from 'src/app/models/IServicio';
 import { Servicio } from 'src/app/models/Servicio';
 import { ServiceService } from 'src/app/services/service.service';
 
@@ -12,18 +13,49 @@ export class HorasTrabajoComponent implements OnInit {
   formularioHoras:FormGroup
   constructor(private fb:FormBuilder, private service:ServiceService) { }
   reportes:Servicio[]
+  iReportes:IServicio[]=[
+    {
+      _idTecnico:"100001",
+      _idServicio:"123456789",
+      _fechaInicio:new Date(1608087300000),
+      _fechaFin:new Date(1608090000000)
+    },
+    {
+      _idTecnico:"100002",
+      _idServicio:"123456789",
+      _fechaInicio:new Date(1608050300000),
+      _fechaFin:new Date(1608090000000)
+    },
+    {
+      _idTecnico:"100003",
+      _idServicio:"123456789",
+      _fechaInicio:new Date(1608085000000),
+      _fechaFin:new Date(1608090000000)
+    },
+    {
+      _idTecnico:"100004",
+      _idServicio:"123456789",
+      _fechaInicio:new Date(1608020300000),
+      _fechaFin:new Date(1608090000000)
+    }
+  ]
+  
+  
+    
+  
   ngOnInit() {
-    this.service.getReportes().subscribe(data=>{
-      this.reportes=data;
-    })
+    
     this.formularioHoras = this.fb.group({
       idTecnico:['',Validators.required],
       numeroSemana:['',Validators.required]
     })
+
   }
 
   consultar(){
-
+    this.service.getReportes().subscribe(data=>{
+      this.reportes=data;
+    })
   }
 
 }
